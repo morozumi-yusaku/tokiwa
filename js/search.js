6d513ede6ff10220f1019a1a8b80389aaa3f1767
tokiwa <00971@hs-tokiwa.ed.jp>
2024-07-16 12:16:17 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -4,7 +4,8 @@
  */
 
 const sleep = waitTime => new Promise( resolve => setTimeout(resolve, waitTime) );
-const FETCH_URL_SEARCH = "https://script.google.com/macros/s/AKfycbzzUbOamX249pRIq0kRwxh_OScPvyF9FAhAJy8FAjpNG8DR7WZFMp_WksYV6Lp7GrLG/exec";
+const FETCH_URL_SEARCH = "https://script.google.com/macros/s/AKfycbzJ4hFOfL_gkQpwcIeRfy0H9ZPU4L_IJ7z-KVJoAVXcPcdOW8EAYZJn2UwkM8naxdDM/exec";
+
 // CSSクラス追加
 function addClass(obj, cls) {
     if (!obj.classList.contains(cls)) {
@@ -56,30 +57,24 @@ async function HandleWordSearch () {
     removeClass(loadScreen, "hidden");
 
     // 値の検証 ========================================
-
     // 値が不適だった場合「検索失敗」へ
     // 検索結果の取得 ========================================
     const url = FETCH_URL_SEARCH + `?type=search&keyword=${ searchWord }&category1=&category2`;
     const fetchs = await fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            return data;
-        });
+        .then(response => response.json())
+        .then(data => data);
 
     if (fetchs.datas.length == 0) {
         // 検索結果がない場合 ========================================
-
         // 検索ワードの反映
         for (let i = 0; i < showSearchWords.length; i++) {
             showSearchWords[i].innerHTML = searchWord;
         }
-    
         // 失敗画面表示
         addClass(beforeScreen, "hidden");
         addClass(loadScreen, "hidden");
         addClass(resultScreen, "hidden");
         removeClass(failScreen, "hidden");
-        
     } else {
         // 検索結果があった場合 ======================================
         const datas = fetchs.datas;
@@ -94,8 +89,8 @@ async function HandleWordSearch () {
             let keywords = "";
 
             for (let j = 0; j < datas[i].keyword.length; j++) {
-                keywords = keywords + `<li class="m-1"><button onclick="HandleTagSearch('${ datas[i].keyword[j] }')" class="bg-purple-600 text-white p-2 py-1 rounded-full">${ datas[i].keyword[j] }</button></li>`;
-            };
+                keywords += `<li class="m-1"><button onclick="HandleTagSearch('${ datas[i].keyword[j] }')" class="bg-purple-600 text-white p-2 py-1 rounded-full">${ datas[i].keyword[j] }</button></li>`;
+            }
 
             li.innerHTML = `
 <li class="py-4">
@@ -110,7 +105,7 @@ async function HandleWordSearch () {
         ${ keywords }
         </ul>
         <div class="flex justify-end">
-            <a href="javascript:openPdf('${ datas[i].pdfUrl }')" class="w-32 text-center bg-yellow-400 hover:bg-yellow-300 text-black p-2 rounded-md shadow-md">開く</a>
+            <a href="${ datas[i].pdfUrl }" target="_blank" class="w-32 text-center bg-yellow-400 hover:bg-yellow-300 text-black p-2 rounded-md shadow-md">開く</a>
         </div>
     </div>
 </li>
@@ -180,75 +175,71 @@ async function HandleCategorySearch(type, ctgry) {
     const url = FETCH_URL_SEARCH + `?type=search&keyword=&category1=${ category1 }&category2=${ category2 }`;
     const fetchs = await fetch(url)
         .then(res => res.json())
-        .then(data => {
-            return data;
-        });
+        .then(data => data);
 
-        console.log(fetchs);
+    if (fetchs.datas.length == 0) {
+        // 検索結果がない場合 ========================================
 
-        if (fetchs.datas.length == 0) {
-            // 検索結果がない場合 ========================================
-    
-            // 検索ワードの反映
-            for (let i = 0; i < showSearchWords.length; i++) {
-                showSearchWords[i].innerHTML = ctgry;
-            }
-        
-            // 失敗画面表示
-            addClass(beforeScreen, "hidden");
-            addClass(loadScreen, "hidden");
-            addClass(resultScreen, "hidden");
-            removeClass(failScreen, "hidden");
-            
-        } else {
-            // 検索結果があった場合 ======================================
-            const datas = fetchs.datas;
-    
-            const ul = document.getElementById("paper-list");
-            // 子要素の削除
-            ul.innerHTML = "";
-    
-            // 検索結果の追加
-            for (let i = 0; i < datas.length; i++) {
-                const li = document.createElement("li");
-                let keywords = "";
-    
-                for (let j = 0; j < datas[i].keyword.length; j++) {
-                    keywords = keywords + `<li class="m-1"><button onclick="HandleTagSearch('${ datas[i].keyword[j] }')" class="bg-purple-600 text-white p-2 py-1 rounded-full">${ datas[i].keyword[j] }</button></li>`;
-                };
-    
-                li.innerHTML = `
-    <li class="py-4">
-        <div class="bg-white rounded-md border p-4">
-            <h3 class="text-2xl font-black mt-2 mb-4"><span class="bg-black p-2 rounded text-white">${ datas[i].type }</span>${ datas[i].title }</h3>
-            <div class="flex mt-1 mb-1">
-                <h4 class="text-lg font-black my-1 mr-4 text-gray-500">大カテゴリー：${ datas[i].category1 }</h4>
-                <h4 class="text-lg font-black my-1 mr-4 text-gray-500">小カテゴリー：${ datas[i].category2.filter(c => c != "") }</h4>
-            </div>
-            <ul class="flex items-center mb-2">
-            <li class="font-bold">キーワード：</li>
-            ${ keywords }
-            </ul>
-            <div class="flex justify-end">
-                <a href="javascript:openPdf('${ datas[i].pdfUrl }')" class="w-32 text-center bg-yellow-400 hover:bg-yellow-300 text-black p-2 rounded-md shadow-md">開く</a>
-            </div>
-        </div>
-    </li>
-                `;
-                ul.appendChild(li);
-            }
-    
-            // 検索ワードの反映
-            for (let i = 0; i < showSearchWords.length; i++) {
-                showSearchWords[i].innerHTML = ctgry;
-            }
-    
-            // 取得画面表示
-            addClass(beforeScreen, "hidden");
-            addClass(loadScreen, "hidden");
-            addClass(failScreen, "hidden");
-            removeClass(resultScreen, "hidden");
+        // 検索ワードの反映
+        for (let i = 0; i < showSearchWords.length; i++) {
+            showSearchWords[i].innerHTML = ctgry;
         }
+
+        // 失敗画面表示
+        addClass(beforeScreen, "hidden");
+        addClass(loadScreen, "hidden");
+        addClass(resultScreen, "hidden");
+        removeClass(failScreen, "hidden");
+        
+    } else {
+        // 検索結果があった場合 ======================================
+        const datas = fetchs.datas;
+
+        const ul = document.getElementById("paper-list");
+        // 子要素の削除
+        ul.innerHTML = "";
+
+        // 検索結果の追加
+        for (let i = 0; i < datas.length; i++) {
+            const li = document.createElement("li");
+            let keywords = "";
+
+            for (let j = 0; j < datas[i].keyword.length; j++) {
+                keywords += `<li class="m-1"><button onclick="HandleTagSearch('${ datas[i].keyword[j] }')" class="bg-purple-600 text-white p-2 py-1 rounded-full">${ datas[i].keyword[j] }</button></li>`;
+            }
+
+            li.innerHTML = `
+<li class="py-4">
+    <div class="bg-white rounded-md border p-4">
+        <h3 class="text-2xl font-black mt-2 mb-4"><span class="bg-black p-2 rounded text-white">${ datas[i].type }</span>${ datas[i].title }</h3>
+        <div class="flex mt-1 mb-1">
+            <h4 class="text-lg font-black my-1 mr-4 text-gray-500">大カテゴリー：${ datas[i].category1 }</h4>
+            <h4 class="text-lg font-black my-1 mr-4 text-gray-500">小カテゴリー：${ datas[i].category2.filter(c => c != "") }</h4>
+        </div>
+        <ul class="flex items-center mb-2">
+        <li class="font-bold">キーワード：</li>
+        ${ keywords }
+        </ul>
+        <div class="flex justify-end">
+            <a href="${ datas[i].pdfUrl }" target="_blank" class="w-32 text-center bg-yellow-400 hover:bg-yellow-300 text-black p-2 rounded-md shadow-md">開く</a>
+        </div>
+    </div>
+</li>
+            `;
+            ul.appendChild(li);
+        }
+
+        // 検索ワードの反映
+        for (let i = 0; i < showSearchWords.length; i++) {
+            showSearchWords[i].innerHTML = ctgry;
+        }
+
+        // 取得画面表示
+        addClass(beforeScreen, "hidden");
+        addClass(loadScreen, "hidden");
+        addClass(failScreen, "hidden");
+        removeClass(resultScreen, "hidden");
+    }
 
     // ボタン有効
     for (let i = 0; i < btns.length; i++) {
